7ce69ea88539b006ca75602433dbfddb0a835530
Daniel Firme <danielfirme.github@gmail.com>
2022-10-23 15:38:46 -0300

    creating core files

---
+++ public/assets/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+$$('header a').forEach(a=>{
+    a.addEventListener('click', (e)=>{
+        e.preventDefault();
+        const positionScroll = $(`${e.target.getAttribute('href')}`).offsetTop;
+        window.scrollTo({
+            top: (positionScroll - 50),
+            left: 0,
+            behavior: "smooth"
+        });
+    });
+});
