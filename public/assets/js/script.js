c4dd1707c091af694bf2d7373c8ca2e17d2c5fda
Elise Firme <elisefirme.github@gmail.com>
2022-10-24 21:29:06 -0300

    update code

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,11 +5,29 @@ const $$ = document.querySelectorAll.bind(document);
 $$('header a').forEach(a=>{
     a.addEventListener('click', (e)=>{
         e.preventDefault();
+        $('.m--active').classList.remove('m--active');
+        e.target.parentNode.classList.add('m--active');
         const positionScroll = $(`${e.target.getAttribute('href')}`).offsetTop;
         window.scrollTo({
-            top: (positionScroll - 60),
+            top: positionScroll,
             left: 0,
             behavior: "smooth"
         });
     });
+});
+
+window.addEventListener('scroll', (e)=>{
+    if(window.scrollY >= 10 && e.clientY >= 80){
+        $('header').style.top = '-80px';
+    }else {
+        $('header').style.top = '0';
+    }
+});
+
+window.addEventListener('mousemove', (e)=>{
+    if(e.clientY >= 80 && window.scrollY >= 10){
+        $('header').style.top = '-80px';
+    }else {
+        $('header').style.top = '0';
+    }
 });
