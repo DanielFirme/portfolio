eab5495eb2c5d0142ee66ad263054deb5d725e88
Daniel Firme <danielfirme.github@gmail.com>
2022-10-25 01:57:17 -0300

    script and html update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 let mousePositionY;
+const screenHeight = $('section').clientHeight;
 
 $$('header a').forEach(a=>{
     a.addEventListener('click', (e)=>{
@@ -18,6 +19,23 @@ $$('header a').forEach(a=>{
 });
 
 window.addEventListener('scroll', ()=>{
+    if(window.scrollY >= 0 && window.scrollY <= 0.5 * screenHeight){
+        $('.m--active').classList.remove('m--active');
+        $('a[href="#hm"]').parentNode.classList.add('m--active');
+    } else if(window.scrollY > 0.5 * screenHeight && window.scrollY <= 1.5 * screenHeight){
+        $('.m--active').classList.remove('m--active');
+        $('a[href="#am"]').parentNode.classList.add('m--active');
+    } else if(window.scrollY > 1.5 * screenHeight && window.scrollY <= 2.5 * screenHeight){
+        $('.m--active').classList.remove('m--active');
+        $('a[href="#tc"]').parentNode.classList.add('m--active');
+    } else if(window.scrollY > 2.5 * screenHeight && window.scrollY <= 3.5 * screenHeight){
+        $('.m--active').classList.remove('m--active');
+        $('a[href="#pj"]').parentNode.classList.add('m--active');
+    } else {
+        $('.m--active').classList.remove('m--active');
+        $('a[href="#ct"]').parentNode.classList.add('m--active');
+    }
+
     if(window.scrollY >= 10 && mousePositionY >= 80){
         $('header').style.top = '-80px';
     }else{
@@ -33,3 +51,4 @@ window.addEventListener('mousemove', (e)=>{
         $('header').style.top = '0';
     }
 });
+
