75a82665338ed908f7959471efd89f81cc998eac
Daniel Firme <danielfirme.github@gmail.com>
2022-11-05 20:39:05 -0300

    slider-projects - first funcitionality finished

--- public/assets/js/slider-projects.js
+++ public/assets/js/slider-projects.js
@@ -1,21 +1,46 @@
 $ = document.querySelector.bind(document); 
 $$ = document.querySelectorAll.bind(document);
 
-window.addEventListener('resize', () => {
-    setWidthSliderItems();
-});
-const totalSlides = $$('.slider--item').length;
+const sliderItems = $$('.slider--item');
+const totalSlides = sliderItems.length;
 const slider = $('.slider--width');
-
-$('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
 const sliderControl = $$('.slider--control');
+const marginSlideConfig = 15;
+let widthSlideItem; 
+let numberSliderItems;
 
-function setWidthSliderItems() {
+slider.style.marginLeft = `${marginSlideConfig / 2}px`;
+sliderItems.forEach(item => {
+    item.style.marginRight = `${marginSlideConfig}px`;
+});
+
+window.addEventListener('resize', () => {
+    numberSliderItems = numberOfSliderItemsRelatedToScreenWidth();
+    setWidthSliderItems(numberSliderItems);
+    setWidthSliderWidth();
+});
+
+function numberOfSliderItemsRelatedToScreenWidth(){
+    const screenWidth = $('body').clientWidth;
+    if(screenWidth >= 840){
+        return 3;
+    } else if (screenWidth < 840 && screenWidth > 430){
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+function setWidthSliderItems(numberSliderItems) {
     const widthSlider = $(".slider").offsetWidth;
-    const widthSlideItem = `${widthSlider / 3}px`;
-    $$(".slider--item").forEach(item => {
-        item.style.width = widthSlideItem;
+    widthSlideItem = (widthSlider / numberSliderItems) - marginSlideConfig;
+    sliderItems.forEach(item => {
+        item.style.width = `${widthSlideItem}px`;
     });
+}
+
+function setWidthSliderWidth() {
+    $('.slider--width').style.width = `${totalSlides * (widthSlideItem + marginSlideConfig)}px`;
 }
 
 function goPrev(){
@@ -29,28 +54,17 @@ function goNext(){
 function updateMargin(control){
     const slides = $$('.slider--item');
     if(control === 1){
-        slides[0].style.marginLeft = `-${$('.slider--item').clientWidth}px`;
+        slides[0].style.marginLeft = `-${$('.slider--item').offsetWidth + marginSlideConfig}px`;
         setTimeout(()=>{
             slider.appendChild(slides[0]);
             $$('.slider--item')[slides.length - 1].style.marginLeft = "0px";
         }, 500);
         
     }
-    // if(control === 2){
-    //     for(let i = 0; i < (slides.length - 1); i++){
-    //         slider.append(slides[i]);
-    //     }
-    //     $$('.slider--item')[0].style.transition = "none";
-    //     $$('.slider--item')[0].style.marginLeft = `-${$('.slider--item').clientWidth}px`;
-    //     setTimeout(()=>{
-    //         $$('.slider--item')[0].style.transition = "all ease 0.5s";
-    //         $$('.slider--item')[0].style.marginLeft = "0px";
-    //     }, 0.00000000000000000000000000000000000000000001);  
-    // }
     if(control === 2){
         slider.prepend(slides[slides.length - 1]);
         $$('.slider--item')[0].style.transition = "none";
-        $$('.slider--item')[0].style.marginLeft = `-${$('.slider--item').clientWidth}px`;
+        $$('.slider--item')[0].style.marginLeft = `-${$('.slider--item').offsetWidth + marginSlideConfig}px`;
         setTimeout(()=>{
             $$('.slider--item')[0].style.transition = "all ease 0.5s";
             $$('.slider--item')[0].style.marginLeft = "0px";
@@ -61,4 +75,6 @@ function updateMargin(control){
 sliderControl[0].addEventListener('click', goPrev);
 sliderControl[1].addEventListener('click', goNext);
 
-setWidthSliderItems();
+numberSliderItems = numberOfSliderItemsRelatedToScreenWidth();
+setWidthSliderItems(numberSliderItems);
+setWidthSliderWidth();
